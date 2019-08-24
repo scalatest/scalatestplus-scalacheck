fc504260208bbaa3d26648c9879234a6c2723323
Chua Chee Seng <cheeseng@amaseng.com>
2019-08-24 21:51:59 +0800

    Bumped up to use scalatest 3.1.0-M1, and commentted out ScalaCheckGenerators and its tests as org.scalatest.prop has been excluded from 3.1 release.

--- scalatestPlusScalaCheck/src/test/scala/org/scalatest/check/ScalaCheckGeneratorsSpec.scala
+++ scalatestPlusScalaCheck/src/test/scala/org/scalatest/check/ScalaCheckGeneratorsSpec.scala
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.scalatestplus.scalacheck
+/*package org.scalatestplus.scalacheck
 
 import org.scalacheck._
 import org.scalatest._
@@ -59,5 +59,5 @@ class ScalaCheckGeneratorsSpec extends funspec.AnyFunSpec with PropertyChecks {
     val scalaTestShrinkList = scalaTestShrinkIt.toList
     scalaTestShrinkList shouldEqual scalaCheckShrinkList.reverse
   }
-}
+}*/
 
